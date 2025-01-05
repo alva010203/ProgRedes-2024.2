f10a47e04dd4fb3b02a94500b3598b2243226360
Jairo <164819215+Jaiir0@users.noreply.github.com>
2025-01-05 00:43:30 -0300

    Delete Unidade01-Avaliacao02/Q1/server/teste.py

--- Unidade01-Avaliacao02/Q1/server/teste.py
+++
@@ -1,9 +0,0 @@
-import os,socket
-import os
-
-caminho_do_arquivo = "Q1/server/files/palavra.txt"
-
-if os.path.exists(caminho_do_arquivo):
-    print("O arquivo existe!")
-else:
-    print("O arquivo NAO existe.")
