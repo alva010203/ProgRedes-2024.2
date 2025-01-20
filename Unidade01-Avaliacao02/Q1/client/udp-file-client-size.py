67830fa32abaf322f6d865bf9ca99ab90e82f491
Álvaro augs <165521280+alva010203@users.noreply.github.com>
2025-01-19 23:15:28 -0300

    Update udp-file-client-size.py
    UDP :)

--- Unidade01-Avaliacao02/Q1/client/udp-file-client-size.py
+++ Unidade01-Avaliacao02/Q1/client/udp-file-client-size.py
@@ -6,41 +6,37 @@ DIRBASE = "files/"
 host = "127.0.0.1"
 port = 3456
 
-# Criação do socket e conexão com o servidor
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Criação do socket UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
-    sock.connect((host, port))
-    print("Conectado ao servidor.")
-
     # Solicita o nome do arquivo
     arq = input('Digite o nome do arquivo: ')
 
-    # Converte o nome do arquivo para bytes e envia o comprimento e o nome
-    lenNameArq = len(arq.encode('utf-8')).to_bytes(2, 'big') #2 bytes do tamanho 
-    msg = lenNameArq + arq.encode()  # Concatena o comprimento e o nome do arquivo
-    sock.send(msg)  
+    # Converte o nome do arquivo para bytes e envia para o servidor
+    msg = arq.encode('utf-8')
+    sock.sendto(msg, (host, port))
 
-    # Espera pela resposta do servidor
-    fileIsOk = int.from_bytes(sock.recv(2), 'big')
-    try:
-        if fileIsOk == 0:
-            tam = int.from_bytes(sock.recv(4), 'big') #recebe 4 bytes do tamanho do arq e converte para inteiro
-            fd = open(DIRBASE+arq, 'wb')
-            while tam > 0: 
-                recBytes = sock.recv(4096)
-                fd.write(recBytes)
-                tam -= len(recBytes) #atualiza o tamanho S
-            fd.close()
-            print("Arquivo recebido com sucesso! ")
-        else:
-            print('Arquivo inacessível')
+    # Recebe a resposta do servidor
+    data, addr = sock.recvfrom(1024)
+    fileIsOk = int.from_bytes(data[:2], 'big')
 
-    except FileNotFoundError as e:
-        print(f"Erro ao criar o arquivo: {e}")
-    except PermissionError as e:
-        print(f"Erro de permissão: {e}")
+    if fileIsOk == 0:
+        # Recebe o tamanho do arquivo
+        tam = int.from_bytes(data[2:6], 'big')
+        fd = open(DIRBASE + arq, 'wb')
 
+        while tam > 0:
+            data, addr = sock.recvfrom(4096)
+            fd.write(data)
+            tam -= len(data)
+        fd.close()
+        print("Arquivo recebido com sucesso!")
+    else:
+        print('Arquivo inacessível')
+
+except KeyboardInterrupt:
+    print("O cliente interrompeu a ação")
 except socket.error as e:
     print(f'Erro de conexão: {e}')
 finally:
