570d5c552be521d29bcc749077b9ad7dcbbc7014
Álvaro augs <165521280+alva010203@users.noreply.github.com>
2025-01-19 23:25:55 -0300

    Update udp-file-server-size.py
    UDP

--- Unidade01-Avaliacao02/Q1/server/udp-file-server-size.py
+++ Unidade01-Avaliacao02/Q1/server/udp-file-server-size.py
@@ -1,34 +1,26 @@
 import socket, os
 
-DIRBASE = "files/" 
-INTERFACE = "" #escutará em todas as interfaces disponiveis 
+DIRBASE = "files/"
+INTERFACE = ""  # Escutará em todas as interfaces disponíveis
 PORT = 3456
 
-myTCPsock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-
-myTCPsock.bind((INTERFACE, PORT))
-myTCPsock.listen(1) #permite apenas uma conexão
-print ("Escutando em ...", (INTERFACE, PORT))
+myUDPsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+myUDPsock.bind((INTERFACE, PORT))
+print("Escutando em ...", (INTERFACE, PORT))
 
 while True:
     # Recebe o nome do arquivo a servir
-    con, cliente = myTCPsock.accept()
-    print('Conectado por: ', cliente)
+    data, addr = myUDPsock.recvfrom(1024)
+    fileName = data.decode('utf-8')
+    print(f"Recebi pedido para o arquivo {fileName} de {addr}")
 
-    mensagem = con.recv(2) #recebe 2 bytes do tamanho 
-    mensagem =  int.from_bytes(mensagem, 'big') #converte para inteiro
-
-    # Abre o arquivo a servir ao cliente
-    fileName = con.recv(mensagem).decode('utf-8') #proximos bytes que contem o nome do arquivo
-    print ("Recebi pedido para o arquivo ", fileName)
-
-    fileName = DIRBASE+fileName
+    fileName = DIRBASE + fileName
     print(f"Caminho completo do arquivo: {fileName}")
 
     if os.path.exists(fileName): #se encontrar o arquivo
-        con.send(b'\x00\x00')
+        myUDPsock.sendto(b'\x00\x00', addr)
         fileSize = os.path.getsize(fileName) #pega o tamanho do arquivo em bytes
-        con.send(fileSize.to_bytes(4, 'big')) #envia em 4 bytes em big endian
+        myUDPsock.sendto(fileSize.to_bytes(4, 'big'), addr) #envia em 4 bytes em big endian
 
         fd = open (fileName, 'rb')
         print ("Enviando arquivo ", fileName)
@@ -37,11 +29,9 @@ while True:
         
         fileData = fd.read(4096) #4kb
         while fileData != b'':
-            con.send(fileData)
+            myUDPsock.sendto(fileData, addr)
             fileData = fd.read(4096)
         # Fecha o arquivo
         fd.close()
     else:
-        con.send(b'\x00\x01') #caso o arquivo não exista
-    con.close()
-myTCPsock.close() #fecha o socket
+        myUDPsock.sendto(b'\x00\x01', addr) #caso o arquivo não exista
